5f02c39c0fa0f1cb895bb0b794d44e49c190af6f
saretotracy <tracysareto@gmail.com>
2022-04-01 22:25:59 +0300

    add django complete registration file

--- gram/urls.py
+++ gram/urls.py
@@ -20,6 +20,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.Welcome,name = 'welcome'),
+    path('page/',views.Page,name = 'page'),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ]
--- insta/urls.py
+++ insta/urls.py
@@ -5,7 +5,7 @@ from django.urls import path,register_converter,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',views.Welcome,name = 'welcome'),
-    path('',views.Page,name = 'page'),
+    path('page/',views.Page,name = 'page'),
     path('accounts/', include('django_registration.backends.one_step.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
     ]
