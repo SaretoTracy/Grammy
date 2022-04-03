571bbbf14d11c17b4cd61e2331af8ccdfbc43e37
saretotracy <tracysareto@gmail.com>
2022-04-03 22:11:39 +0300

    add comment module

--- insta/models.py
+++ insta/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from cloudinary.models import CloudinaryField
-
+from django.utils import timezone
 
 class Profile (models.Model):
     profile_pic= CloudinaryField('images/')
@@ -13,6 +13,15 @@ class Profile (models.Model):
         self.save()
     def delete_profile(self):
         self.delete()
+class Comments(models.Model):
+    '''
+    Class that handles user comments
+    '''
+    message = models.TextField()
+    pub_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.message
 
 
 class Photos(models.Model):
@@ -20,7 +29,9 @@ class Photos(models.Model):
     image = CloudinaryField('images/')
     caption= models.CharField(max_length=250)
     profile =  models.ManyToManyField('profile')
-    Comments= models.CharField(max_length=250)
+    likes = models.PositiveIntegerField(default=0)
+    date = models.DateTimeField(default=timezone.now)
+    comments = models.ForeignKey(Comments, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.caption
@@ -29,3 +40,10 @@ class Photos(models.Model):
         self.save()
     def delete_photos(self):
         self.delete()
+    @classmethod
+    def display_all(cls):
+        '''
+        Function that dislays post information
+        '''
+        caption = cls.objects.all()
+        return caption
--- insta/views.py
+++ insta/views.py
@@ -7,16 +7,17 @@ from django.contrib.auth.decorators import login_required
 # Create your views here.
 def Welcome(request):
 
-    # Function that gets the date
-    post = Photos.objects.all()
+    
 
-    return render(request, 'welcome.html',{'posts':post})
+    return render(request, 'welcome.html')
 
 
 @login_required(login_url='/accounts/login/')
 def Page(request):
+    # Function that gets the date
+    post = Photos.objects.all()
 
     
   
     
-    return render(request, 'page.html')
+    return render(request, 'page.html',{'posts':post})
