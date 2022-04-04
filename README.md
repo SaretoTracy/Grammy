9d46129b394959ced18498a103feb612be58b7dc
saretotracy <tracysareto@gmail.com>
2022-04-04 14:32:54 +0300

    add readme

---
+++ README.md
@@ -0,0 +1,91 @@
+# BLOG POST
+
+Instagram clone application .Users can post photos,like and comment on ither users photo.
+
+## Technologies Used
+
+The following languages have been used on this project:
+
+- HTML
+- CSS
+- Python
+- Bootstrap
+- Django
+
+## Live Demo
+
+[Live Demo Link] https:
+
+## Getting Started
+
+To get a local copy, follow these simple example steps.
+
+### Prerequisites
+
+A computer with a working and up to date web browser.
+
+## Setup Installation
+
+- Copy the github repository url
+- Clone to your computer
+- Open terminal and navigate to the directory of the project you just cloned to your computer
+- Run the following command to start the server using virtual environment
+
+```
+python3.8 -m venv --without-pip virtual
+```
+
+- To activate the virtual environment
+
+```
+source venv/bin/activate
+```
+
+```
+curl https://bootstrap.pypa.io/get-pip.py | python
+```
+
+```
+pip install django
+```
+
+```
+chmod a+x start.sh
+```
+
+```
+./start.sh
+```
+
+- open the browser and navigate to http://127.0.0.1:8000/ to see the application in action
+
+### BDD
+
+## Features and BDD
+
+1️⃣ Sign in to the application to start using.
+2️⃣ Upload my pictures to the application.
+3️⃣See my profile with all my pictures.
+4️⃣Follow other users and see their pictures on my timeline.
+5️⃣ Like a picture and leave a comment on it.
+
+## Author
+
+👤 **Author**
+
+By Tracy Chepkorir
+
+- GitHub: [@saretotracy](https://github.com/saretotracy)
+- email: <a href="mailto:tracychepkorir99@gmailcom"> mail📪</a>
+
+## Show your support
+
+Give a ⭐️ if you like this project!
+
+## Acknowledgments
+
+- Appreciation to Moringa school for giving me this opportunity to learn 😊
+
+## 📝 License
+
+This project is [MIT](LICENSE) licensed.
--- gram/settings.py
+++ gram/settings.py
@@ -12,29 +12,23 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 import os
-import django_heroku
-import dj_database_url
-from decouple import config,Csv
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-# BASE_DIR = Path(__file__).resolve().parent.parent
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+z
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = 'django-insecure-3l7u^dd39nau-29xq#b237!vvuk!2xu+#3^qs((1@f#r3wkf7s'
 
-MODE=config("MODE", default="dev")
-SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
-# DEBUG = True
-DEBUG = config('DEBUG', default=False, cast=bool)
+DEBUG = True
 
-# ALLOWED_HOSTS = []
+ALLOWED_HOSTS = []
 
 
 # Application definition
@@ -60,7 +54,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
 ]
 
 ROOT_URLCONF = 'gram.urls'
@@ -88,29 +81,14 @@ WSGI_APPLICATION = 'gram.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-if config('MODE')=="dev":
-   DATABASES = {
-       'default': {
-           'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           'NAME': config('DB_NAME'),
-           'USER': config('DB_USER'),
-           'PASSWORD': config('DB_PASSWORD'),
-           'HOST': config('DB_HOST'),
-           'PORT': '',
-       }
-       
-   }
-# production
-else:
-   DATABASES = {
-       'default': dj_database_url.config(
-           default=config('DATABASE_URL')
-       )
-   }
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', cast=Csv())
+DATABASES = {
+ 'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'gram',
+        'USER': 'moringa',
+        'PASSWORD':'kimachas',
+    }
+}
 
 cloudinary.config( 
   cloud_name = "dpusync50", 
@@ -152,7 +130,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
@@ -161,8 +139,6 @@ STATICFILES_DIRS = [
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-# Configure Django App for Heroku.
-django_heroku.settings(locals())
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
